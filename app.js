68fda1e2889a2f34bec77ff7da6f7f3446e48ee4
Yirong Qin <yq4103@mun.ca>
2022-03-28 23:04:27 -0400

    appjs file update

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const port = 3000;
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/js', express.static(__dirname + 'public/js'));
+
 
 app.get('', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
